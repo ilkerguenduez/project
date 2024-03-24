c1846ebecec0e29bea26971bf074234aca80517c
İlker Gündüz <ilkergndz3500@gmail.com>
2024-03-24 18:15:29 +0300

    posts migration recognized

--- database/migrations/2024_03_24_150949_create_posts_table.php
+++ database/migrations/2024_03_24_150949_create_posts_table.php
@@ -13,6 +13,9 @@ return new class extends Migration
     {
         Schema::create('posts', function (Blueprint $table) {
             $table->id();
+            $table->unsignedInteger('user_id');
+            $table->string('title');
+            $table->string('body');
             $table->timestamps();
         });
     }
