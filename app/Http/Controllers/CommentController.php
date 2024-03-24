94fb0576abb7f6c2215e2d230f9f07133409a191
İlker Gündüz <ilkergndz3500@gmail.com>
2024-03-24 19:40:30 +0300

    Comment Controller function added

--- app/Http/Controllers/CommentController.php
+++ app/Http/Controllers/CommentController.php
@@ -3,8 +3,12 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Comment;
 
 class CommentController extends Controller
 {
-    //
+    public function index(){
+        $comments = Comment::all();
+        return response()->json($comments);
+    }
 }
