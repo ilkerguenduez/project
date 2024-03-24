593c3a4150268b717eca439358139376d3f52c2b
İlker Gündüz <ilkergndz3500@gmail.com>
2024-03-24 19:41:55 +0300

    routes added and testing done

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,13 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PostController;
+use App\Http\Controllers\CommentController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/posts',[PostController::class,'index']);
+Route::get('/comments',[CommentController::class,'index']);
+Route::get('/posts/{post}/comments',[PostController::class,'comments']);
