aef2a7a10d878486627397b17ab63097182c6d41
İlker Gündüz <ilkergndz3500@gmail.com>
2024-03-24 18:31:07 +0300

    data too long error and body type changed to text

--- database/migrations/2024_03_24_150954_create_comments_table.php
+++ database/migrations/2024_03_24_150954_create_comments_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
             $table->unsignedInteger('post_id');
             $table->string('name');
             $table->string('email');
-            $table->string('body');
+            $table->text('body');
             $table->timestamps();
         });
     }
