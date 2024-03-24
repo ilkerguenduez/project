ea6badef858b3970af4aa12626b283e05bc35c3b
İlker Gündüz <ilkergndz3500@gmail.com>
2024-03-24 19:41:42 +0300

    Post Controller function added

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -3,8 +3,17 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Post;
 
 class PostController extends Controller
 {
-    //
+    public function index(){
+        $posts = Post::all();
+        return response()->json($posts);
+    }
+
+    public function comments(Post $post){
+        $comments = $post->comments;
+        return response()->json($comments);
+    }
 }
